cdf9eed796c9eaa634d410b058ffb7679927d803
marciomaiabr <marcio.luis.maia@gmail.com>
2023-12-19 06:12:00 -0400

    added basic jsf20 project

---
+++ projects/a202312190523/src/main/java/pkgs/mbs/teste01/Teste01.java
@@ -0,0 +1,13 @@
+package pkgs.mbs.teste01;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Teste01 {
+
+	public String testa() {
+		System.out.println("Teste01.testa()... 202312190547...");
+		return null;
+	}
+	
+}
