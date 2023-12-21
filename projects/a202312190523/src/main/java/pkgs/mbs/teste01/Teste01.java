22c8947f6223226dd920efd57c7a6c0dacc79352
marciomaiabr <marcio.luis.maia@gmail.com>
2023-12-21 07:23:21 -0400

    testing @ViewScoped

--- projects/a202312190523/src/main/java/pkgs/mbs/teste01/Teste01.java
+++ projects/a202312190523/src/main/java/pkgs/mbs/teste01/Teste01.java
@@ -1,13 +1,38 @@
 package pkgs.mbs.teste01;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import pkgs.models.Produto;
 
 @ManagedBean
-public class Teste01 {
+@ViewScoped
+public class Teste01 implements Serializable {
 
-	public String testa() {
-		System.out.println("Teste01.testa()... 202312200448...");
-		return null;
+	public Teste01() {
+		System.out.println("Teste01.Teste01()");
+		this.produtos = new ArrayList<>();
+		this.produto = new Produto();
 	}
-	
+
+	private Produto produto;
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	private List<Produto> produtos;
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void incluir() {
+		produtos.add(this.produto);
+		this.produto = new Produto();
+	}
+
 }
