316a855df4bd6e94ef0e92d7a6d32ebca7a3d886
marciomaiabr <marcio.luis.maia@gmail.com>
2023-12-22 09:38:29 -0400

    basico 202312220938

--- projects/a202312190523/src/main/java/pkgs/mbs/teste01/Teste01.java
+++ projects/a202312190523/src/main/java/pkgs/mbs/teste01/Teste01.java
@@ -7,12 +7,12 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 
 import pkgs.models.Produto;
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class Teste01 implements Serializable {
 
 	public Teste01() {
