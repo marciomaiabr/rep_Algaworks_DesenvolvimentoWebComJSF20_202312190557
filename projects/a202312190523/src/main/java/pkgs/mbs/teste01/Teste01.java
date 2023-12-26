42a15c2c5d7786adb44d26046441eca70d198309
marciomaiabr <marcio.luis.maia@gmail.com>
2023-12-26 05:23:16 -0400

    basic 202312260523

--- projects/a202312190523/src/main/java/pkgs/mbs/teste01/Teste01.java
+++ projects/a202312190523/src/main/java/pkgs/mbs/teste01/Teste01.java
@@ -12,41 +12,11 @@ import javax.faces.bean.SessionScoped;
 public class Teste01 implements Serializable {
 
 	public long getMbTs() {
-		return 202312260424L;
+		return 202312260521L;
 	}
 
 	public Teste01() {
 		System.out.println("Teste01.Teste01()");
-	}
-
-	private String vaga = "A0382D";
-
-	public String getVaga() {
-		return vaga;
-	}
-
-	public void setVaga(String vaga) {
-		this.vaga = vaga;
-	}
-
-	private String cargo = "Programador Java Senior";
-
-	public String getCargo() {
-		return cargo;
-	}
-
-	public void setCargo(String cargo) {
-		this.cargo = cargo;
-	}
-
-	private boolean ativa = true;
-
-	public boolean isAtiva() {
-		return ativa;
-	}
-
-	public void setAtiva(boolean ativa) {
-		this.ativa = ativa;
 	}
 
 	@PostConstruct
