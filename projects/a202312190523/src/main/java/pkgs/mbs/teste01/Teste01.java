bc14d5278f9af0510a39bd120e08eeb82ae87a10
marciomaiabr <marcio.luis.maia@gmail.com>
2023-12-29 05:46:06 -0400

    testing converters

--- projects/a202312190523/src/main/java/pkgs/mbs/teste01/Teste01.java
+++ projects/a202312190523/src/main/java/pkgs/mbs/teste01/Teste01.java
@@ -1,14 +1,18 @@
 package pkgs.mbs.teste01;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.ViewScoped;
+
+import pkgs.models.ItemEstoque;
 
 @ManagedBean
-@SessionScoped
+@ViewScoped
 public class Teste01 implements Serializable {
 
 	public Teste01() {
@@ -19,9 +23,33 @@ public class Teste01 implements Serializable {
 		return 202312280629L;
 	}
 
+	private ItemEstoque itemEstoque;
+
+	public ItemEstoque getItemEstoque() {
+		return itemEstoque;
+	}
+
+	public void setItemEstoque(ItemEstoque itemEstoque) {
+		this.itemEstoque = itemEstoque;
+	}
+
+	private List<ItemEstoque> listaItemEstoque;
+
+	public List<ItemEstoque> getListaItemEstoque() {
+		return listaItemEstoque;
+	}
+
+	public void incluir() {
+		System.out.println("Teste01.incluir()");
+		getListaItemEstoque().add(getItemEstoque());
+		setItemEstoque(new ItemEstoque());
+	}
+
 	@PostConstruct
 	public void init() {
 		System.out.println("Teste01.init()");
+		this.itemEstoque = new ItemEstoque();
+		this.listaItemEstoque = new ArrayList<>();
 	}
 
 	@PreDestroy
