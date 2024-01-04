50b29a122dea3f09a86b43cd443369495d17644a
marciomaiabr <marcio.luis.maia@gmail.com>
2024-01-04 07:29:58 -0400

    5.7. Exclusão de lançamentos do banco de dados

--- projects/a202312190523/src/main/java/com/algaworks/cursojsf2/financeiro/view/ConsultaLancamentoBean.java
+++ projects/a202312190523/src/main/java/com/algaworks/cursojsf2/financeiro/view/ConsultaLancamentoBean.java
@@ -5,18 +5,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.criterion.Order;
 
 import com.algaworks.cursojsf2.financeiro.model.Lancamento;
+import com.algaworks.cursojsf2.financeiro.util.FacesUtil;
 import com.algaworks.cursojsf2.financeiro.util.HibernateUtil;
 
 @ManagedBean
 public class ConsultaLancamentoBean implements Serializable {
 
 	private List<Lancamento> lancamentos = new ArrayList<Lancamento>();
+	private Lancamento lancamentoSelecionado;
 	
 	@SuppressWarnings("unchecked")
 	@PostConstruct
@@ -30,8 +35,34 @@ public class ConsultaLancamentoBean implements Serializable {
 		session.close();
 	}
 
+	public void excluir() {
+		if (this.lancamentoSelecionado.isPago()) {
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, "Lançamento já foi pago e não pode ser excluído.");
+		} else {
+			Session session = HibernateUtil.getSession();
+			Transaction trx = session.beginTransaction();
+			
+			session.delete(this.lancamentoSelecionado);
+			
+			trx.commit();
+			session.close();
+			
+			this.inicializar();
+			
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_INFO, "Lançamento excluído com sucesso!");
+		}
+	}
+	
 	public List<Lancamento> getLancamentos() {
 		return lancamentos;
+	}
+
+	public Lancamento getLancamentoSelecionado() {
+		return lancamentoSelecionado;
+	}
+
+	public void setLancamentoSelecionado(Lancamento lancamentoSelecionado) {
+		this.lancamentoSelecionado = lancamentoSelecionado;
 	}
 	
 }
