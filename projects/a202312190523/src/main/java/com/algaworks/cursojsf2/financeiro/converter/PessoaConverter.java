146458bab76c8385e758a291985f27ad709bfc5a
marciomaiabr <marcio.luis.maia@gmail.com>
2024-01-04 07:36:47 -0400

    5.8. Transações e o pattern Open Session in View

--- projects/a202312190523/src/main/java/com/algaworks/cursojsf2/financeiro/converter/PessoaConverter.java
+++ projects/a202312190523/src/main/java/com/algaworks/cursojsf2/financeiro/converter/PessoaConverter.java
@@ -8,7 +8,7 @@ import javax.faces.convert.FacesConverter;
 import org.hibernate.Session;
 
 import com.algaworks.cursojsf2.financeiro.model.Pessoa;
-import com.algaworks.cursojsf2.financeiro.util.HibernateUtil;
+import com.algaworks.cursojsf2.financeiro.util.FacesUtil;
 
 @FacesConverter(forClass=Pessoa.class)
 public class PessoaConverter implements Converter {
@@ -18,11 +18,9 @@ public class PessoaConverter implements Converter {
 		Pessoa retorno = null;
 		
 		if (value != null) {
-			Session session = HibernateUtil.getSession();
+			Session session = (Session) FacesUtil.getRequestAttribute("session");
 			
 			retorno = (Pessoa) session.get(Pessoa.class, new Integer(value));
-			
-			session.close();
 		}
 		
 		return retorno;
--- projects/a202312190523/src/main/java/com/algaworks/cursojsf2/financeiro/view/ConsultaLancamentoBean.java
+++ projects/a202312190523/src/main/java/com/algaworks/cursojsf2/financeiro/view/ConsultaLancamentoBean.java
@@ -7,15 +7,12 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
 
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.criterion.Order;
 
 import com.algaworks.cursojsf2.financeiro.model.Lancamento;
 import com.algaworks.cursojsf2.financeiro.util.FacesUtil;
-import com.algaworks.cursojsf2.financeiro.util.HibernateUtil;
 
 @ManagedBean
 public class ConsultaLancamentoBean implements Serializable {
@@ -26,26 +23,19 @@ public class ConsultaLancamentoBean implements Serializable {
 	@SuppressWarnings("unchecked")
 	@PostConstruct
 	public void inicializar() {
-		Session session = HibernateUtil.getSession();
+		Session session = (Session) FacesUtil.getRequestAttribute("session");
 		
 		this.lancamentos = session.createCriteria(Lancamento.class)
 				.addOrder(Order.desc("dataVencimento"))
 				.list();
-		
-		session.close();
 	}
 
 	public void excluir() {
 		if (this.lancamentoSelecionado.isPago()) {
 			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, "Lançamento já foi pago e não pode ser excluído.");
 		} else {
-			Session session = HibernateUtil.getSession();
-			Transaction trx = session.beginTransaction();
-			
+			Session session = (Session) FacesUtil.getRequestAttribute("session");
 			session.delete(this.lancamentoSelecionado);
-			
-			trx.commit();
-			session.close();
 			
 			this.inicializar();
 			
