4eddfbf81dee19083da35ec6576e3c2ef742cd0a
marciomaiabr <marcio.luis.maia@gmail.com>
2024-01-12 06:33:37 -0400

    testing JPA perform

--- projects/a202312190523/src/main/java/com/algaworks/cursojsf2/financeiro/exes/Exe01.java
+++ projects/a202312190523/src/main/java/com/algaworks/cursojsf2/financeiro/exes/Exe01.java
@@ -5,12 +5,22 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import com.algaworks.cursojsf2.financeiro.model.Pessoa;
 
 public class Exe01 {
 
 	public static void main(String... args) {
 		System.out.println("Exe01.main()");
-		tstJDBCTime();
+		// tstJDBCTime();
+		//
+		tstJPATime();
 	}
 
 	private static void tstJDBCTime() {
@@ -56,6 +66,50 @@ public class Exe01 {
 		System.out.println("[S:ldtF-ldtI=" + (((ldtF.getNano() - ldtI.getNano()) / 1000000d) / 1000d) + "]");
 		System.out.println("[ldtF.getSecond()-ldtI.getSecond()=" + (ldtF.getSecond() - ldtI.getSecond()) + "]");
 		System.out.println("/Exe01.tstJDBCTime()");
+	}
+
+	private static void tstJPATime() {
+		System.out.println("Exe01.tstJPATime()");
+		LocalDateTime ldtI = LocalDateTime.now();
+		EntityManagerFactory entityManagerFactory = null;
+		EntityManager entityManager = null;
+		try {
+			String pu = null;
+			pu = "myPU";
+			// pu = "tstPU1";
+			entityManagerFactory = Persistence.createEntityManagerFactory(pu);
+			calculaTime(ldtI, null);
+			entityManager = entityManagerFactory.createEntityManager();
+			calculaTime(ldtI, null);
+			TypedQuery<Pessoa> typedQuery = entityManager.createQuery("from Pessoa", Pessoa.class);
+			calculaTime(ldtI, null);
+			List<Pessoa> list = typedQuery.getResultList();
+			list.forEach(System.out::println);
+			calculaTime(ldtI, null);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				entityManager.close();
+			} catch (Exception e2) {
+			}
+			try {
+				entityManagerFactory.close();
+			} catch (Exception e2) {
+			}
+		}
+		calculaTime(ldtI, null);
+		System.out.println("/Exe01.tstJPATime()");
+	}
+
+	private static void calculaTime(LocalDateTime ldtI, LocalDateTime ldtF) {
+		if(ldtF == null)
+			ldtF = LocalDateTime.now();
+
+		System.out.println("[Ns:ldtF-ldtI=" + (ldtF.getNano() - ldtI.getNano()) + "]");
+		System.out.println("[Ms:ldtF-ldtI=" + ((ldtF.getNano() - ldtI.getNano()) / 1000000d) + "]");
+		System.out.println("[S:ldtF-ldtI=" + (((ldtF.getNano() - ldtI.getNano()) / 1000000d) / 1000d) + "]");
+		System.out.println("[ldtF.getSecond()-ldtI.getSecond()=" + (ldtF.getSecond() - ldtI.getSecond()) + "]");
 	}
 
 }
