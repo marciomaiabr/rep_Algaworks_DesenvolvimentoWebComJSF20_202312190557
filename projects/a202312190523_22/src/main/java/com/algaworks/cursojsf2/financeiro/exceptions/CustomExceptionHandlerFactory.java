59c28067eb46fdfe30dead7352b466f8687bd5cd
Administrador <Administrador@DESKTOP-HARG5BV>
2024-01-16 09:42:05 -0400

    adding Custom Exception Handler

---
+++ projects/a202312190523_22/src/main/java/com/algaworks/cursojsf2/financeiro/exceptions/CustomExceptionHandlerFactory.java
@@ -0,0 +1,21 @@
+package com.algaworks.cursojsf2.financeiro.exceptions;
+
+import javax.faces.context.ExceptionHandler;
+import javax.faces.context.ExceptionHandlerFactory;
+
+public class CustomExceptionHandlerFactory extends ExceptionHandlerFactory {
+
+    private ExceptionHandlerFactory exceptionHandlerFactory;
+
+    public CustomExceptionHandlerFactory() {
+    }
+
+    public CustomExceptionHandlerFactory(ExceptionHandlerFactory exceptionHandlerFactory) {
+        this.exceptionHandlerFactory = exceptionHandlerFactory;
+    }
+
+    @Override
+    public ExceptionHandler getExceptionHandler() {
+        return new CustomExceptionHandler(exceptionHandlerFactory.getExceptionHandler());
+    }
+}
