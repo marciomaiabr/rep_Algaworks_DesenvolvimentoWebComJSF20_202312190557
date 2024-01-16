03bf2547b0efbfe054792a022e36fa9859bb966d
Administrador <Administrador@DESKTOP-HARG5BV>
2024-01-16 05:05:36 -0400

    Servindo arquivos para download com JSF

--- projects/a202312190523_22/src/main/java/com/algaworks/cursojsf2/financeiro/view/ConsultaLancamentoBean.java
+++ projects/a202312190523_22/src/main/java/com/algaworks/cursojsf2/financeiro/view/ConsultaLancamentoBean.java
@@ -1,5 +1,9 @@
 package com.algaworks.cursojsf2.financeiro.view;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,6 +11,8 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 import com.algaworks.cursojsf2.financeiro.model.Lancamento;
 import com.algaworks.cursojsf2.financeiro.repository.Lancamentos;
@@ -39,6 +45,40 @@ public class ConsultaLancamentoBean implements Serializable {
 		} catch (RegraNegocioException e) {
 			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, e.getMessage());
 		}
+	}
+	
+	public void downloadComprovante() throws IOException {
+		if(!lancamentoSelecionado.isPago() || lancamentoSelecionado.getComprovante() == null) {
+			FacesUtil.adicionarMensagem(FacesMessage.SEVERITY_ERROR, "Lançamento não possui comprovante!");
+			return;
+		}
+		
+		String nomeArquivo = "comprovante_" + lancamentoSelecionado.getCodigo() + ".pdf";
+		
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		
+		externalContext.responseReset();
+		externalContext.setResponseContentType("application/pdf");
+		externalContext.setResponseHeader("Content-Disposition", "attachment; filename=\""+nomeArquivo+"\"");
+		
+		OutputStream out = externalContext.getResponseOutputStream();
+		
+		try(
+				InputStream is = new ByteArrayInputStream(lancamentoSelecionado.getComprovante())
+				){
+			
+			int read = -1;
+			byte [] bytes = new byte[1024];
+			
+			while((read = is.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			
+		}
+		
+		facesContext.responseComplete();
+		
 	}
 	
 	public List<Lancamento> getLancamentos() {
